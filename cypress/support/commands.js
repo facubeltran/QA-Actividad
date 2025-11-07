099d4e6e54c10aa825e1b3301859d0b7b0c10219
facubeltran <facundocristianbeltran@gmail.com>
2025-11-07 18:17:49 -0300

    primer ejercicio done

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,39 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('loginId',(nombre,apellido,telefono,dni) =>{
+    
+    cy.log('Ingresando nombres, apellidos, celular y dni')
+    cy.get('[data-cy="input-nombres"]').clear().type(nombre)
+    cy.get('[data-cy="input-apellido"]').clear().type(apellido)
+    cy.get('[data-cy="input-telefono"]').clear().type(telefono)
+    cy.get('[data-cy="input-dni"]').clear().type(dni)
+})
+
+Cypress.Commands.add('loginUbicacion',(provincia,localidad)=>{
+    cy.log('Seleccionar provincia y localidad')
+    cy.get('[data-cy="select-provincia"]').clear().type(provincia)
+    cy.get('ul > li > span').contains('Córdoba').click()
+    cy.get('[data-cy="select-localidad"]').clear().type(localidad)
+    cy.get('ul > li > span').contains('Córdoba').click()
+})
+
+Cypress.Commands.add('loginBirth',(day,month,year)=>{
+    cy.log('Ingresando fecha de nacimiento')
+    cy.get('[data-cy="input-fecha-nacimiento"] [data-type="day"]').clear().type(day)
+    cy.get('[data-cy="input-fecha-nacimiento"] [data-type="month"]').clear().type(month)
+    cy.get('[data-cy="input-fecha-nacimiento"] [data-type="year"]').clear().type(year)
+
+})
+
+Cypress.Commands.add('loginEmailPass',(email,pass)=>{
+    cy.log('Ingresando mail y confirmación')
+    cy.get('[data-cy="input-email"]').clear().type(email)
+    cy.get('[data-cy="input-confirmar-email"]').clear().type(email)
+
+    cy.log('Ingresando contraseña y confirmación')
+    cy.get('[data-cy="input-password"]').clear().type(pass)
+    cy.get('[data-cy="input-repetir-password"]').clear().type(pass)
+})
