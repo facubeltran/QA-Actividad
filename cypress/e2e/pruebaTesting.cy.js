dd9720295f6f6ace452418c06b41e477b2d16eda
facubeltran <facundocristianbeltran@gmail.com>
2025-11-07 18:53:14 -0300

    3er ejercicio done

--- cypress/e2e/pruebaTesting.cy.js
+++ cypress/e2e/pruebaTesting.cy.js
@@ -19,7 +19,19 @@ describe('Formulario de Registro', () => {
         cy.log('Enviar formulario')
         cy.get('[data-cy="btn-registrarse"]').click().wait(2000)
         
-        cy.get('[data-cy="error-message"]').should('be.visible').contains('Ya existe un usuario registrado con ese correo electrónico')
+        cy.get('[data-cy="error-message"]').should('be.visible').and('contain','Ya existe un usuario registrado con ese correo electrónico')
+
+    })
+
+        it.skip('Completa todos los campo,presiona Registrar y test dni duplicado', () => {
+        cy.loginId('Juan','Pérez','3511234567','20268800')
+        cy.loginUbicacion('Córdoba','Córdoba')
+        cy.loginBirth('15','08','1995')
+        cy.loginEmailPass('juan.perez90@example.com','P@ssw0rd123')
+        cy.log('Enviar formulario')
+        cy.get('[data-cy="btn-registrarse"]').click().wait(2000)
+        
+        cy.get('[data-cy="error-message"]').should('be.visible').contains('DNI')
 
     })
 })
