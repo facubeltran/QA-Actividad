3f03ded6a59e25486d61119f9ba43893dab3bc8d
facubeltran <facundocristianbeltran@gmail.com>
2025-11-07 18:46:18 -0300

    2do ejercicio done

--- cypress/e2e/pruebaTesting.cy.js
+++ cypress/e2e/pruebaTesting.cy.js
@@ -2,13 +2,24 @@ describe('Formulario de Registro', () => {
     beforeEach(() => {
         cy.visit('https://ticketazo.com.ar/auth/registerUser')
     })
-    it('Completa todos los campos y presiona Registrar', () => {
+    it.skip('Completa todos los campos y presiona Registrar', () => {
         cy.loginId('Juan','Pérez','3511234567','20268800')
         cy.loginUbicacion('Córdoba','Córdoba')
         cy.loginBirth('15','08','1995')
         cy.loginEmailPass('juan.perez90@example.com','P@ssw0rd123')
         cy.log('Enviar formulario')
-        // cy.get('[data-cy="btn-registrarse"]').click().wait(2000)
+        cy.get('[data-cy="btn-registrarse"]').click().wait(2000)
     })
 
+    it('Completa todos los campo,presiona Registrar y test email duplicado', () => {
+        cy.loginId('Juan','Pérez','3511234567','21268800')
+        cy.loginUbicacion('Córdoba','Córdoba')
+        cy.loginBirth('15','08','1995')
+        cy.loginEmailPass('juan.perez90@example.com','P@ssw0rd123')
+        cy.log('Enviar formulario')
+        cy.get('[data-cy="btn-registrarse"]').click().wait(2000)
+        
+        cy.get('[data-cy="error-message"]').should('be.visible').contains('Ya existe un usuario registrado con ese correo electrónico')
+
+    })
 })
